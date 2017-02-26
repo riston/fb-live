cfc7ff549cd91ab76fdcc5656175872e58c14f68
riston <est.risto@gmail.com>
2017-02-27 00:38:37 +0200

    Disable ecto

--- config/dev.exs
+++ config/dev.exs
@@ -33,10 +33,10 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
-config :fb_live, FbLive.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "fb_live_dev",
-  hostname: "localhost",
-  pool_size: 10
+# config :fb_live, FbLive.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: "postgres",
+#   password: "postgres",
+#   database: "fb_live_dev",
+#   hostname: "localhost",
+#   pool_size: 10
--- lib/fb_live/repo.ex
+++ lib/fb_live/repo.ex
@@ -1,3 +1,3 @@
 defmodule FbLive.Repo do
-  use Ecto.Repo, otp_app: :fb_live
+  # use Ecto.Repo, otp_app: :fb_live
 end
