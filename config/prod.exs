a849b27f6cc9a9b8a9a465c26787cc48c3b4dae5
riston <est.risto@gmail.com>
2017-03-19 23:23:20 +0200

    Log level for production change

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :fb_live, FbLive.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
